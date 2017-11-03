01ff375a3e3082508b8fa7e3ff0a4e85838ed282
Unknown <weinert.marlon@googlemail.com>
2017-11-03 12:56:03 +0100

    removed intersphinx package (not needed)

--- source/conf.py
+++ source/conf.py
@@ -33,7 +33,6 @@ sys.path.insert(0, os.path.abspath('.'))
 # ones.
 extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.doctest',
-              'sphinx.ext.intersphinx',
               'sphinx.ext.todo',
               'sphinx.ext.coverage',
               'sphinx.ext.imgmath',
